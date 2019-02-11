8f6cbb650ef3e996ff700224da2f1ad25b008a92
task4233 <tks.m1205@gmail.com>
2019-02-11 04:52:18 +0000

    [FIXED] page layouts(bootstrap)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190206040839) do
+ActiveRecord::Schema.define(version: 20190210004817) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20190206040839) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "deadline"
+    t.string "status"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
@@ -37,6 +39,7 @@ ActiveRecord::Schema.define(version: 20190206040839) do
     t.string "provider"
     t.string "uid"
     t.string "username"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
